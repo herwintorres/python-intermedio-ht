91a990de3a11236b85109d3ab2a88bfec2eabdeb
herwintorres <hertosan29@gmail.com>
2022-06-05 19:12:00 -0500

    python avanzado

--- datetimep.py
+++ datetimep.py
@@ -4,6 +4,14 @@ def run():
     print(my_time)
     # my_day = datetime.date.today()
     # print(my_day)
+    my_str = my_time.strftime('%d/%m/%Y')
+    print(f'Formato LATA: {my_str}')
+
+    my_str = my_time.strftime('%m/%d/%Y')
+    print(f'Formato USA: {my_str}')
+
+    my_str = my_time.strftime('Estamos en el año %Y')
+    print(f'Formato Random: {my_str}')
 
 if __name__ == '__main__':
     run()
